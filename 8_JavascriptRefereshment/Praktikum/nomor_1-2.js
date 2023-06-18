afc51e5b4ed91b7fca704c3f0198ccd91c17aec8
SidikR <rahmad.119140096@student.itera.ac.id>
2023-06-19 05:53:29 +0700

    commit nomor 1 - task 8_JavascriptRefreshment

---
+++ 8_JavascriptRefereshment/Praktikum/nomor_1-2.js
@@ -0,0 +1,25 @@
+let a = 5
+let b = "kampus merdeka"
+const nama = "budi"
+let terdaftar = false;
+let lengkap_arr = [a, b, nama, terdaftar]
+
+function perkenalan() {
+    let asal = "indonesia"
+    return console.log(
+        "perkenalkan nama saya " + nama + " nomor urut " + 
+        a + " sekarang sedang mengikuti " +
+        b + " berasal dari " + asal
+    )
+}
+
+if (terdaftar === true) {
+    console.log(nama + " terdaftar sebagai kegiatan kampus merdeka")
+}
+
+a = b;
+nama = b ;
+
+console.log("asal diakses " + asal);
+console.log("a adalah = " + a)
+console.log("b adalah = " + b)
