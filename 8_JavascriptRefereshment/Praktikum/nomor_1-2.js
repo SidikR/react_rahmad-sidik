e2b209e2d7d09b53e4afece9809c236ee505b95a
SidikR <rahmad.119140096@student.itera.ac.id>
2023-06-19 06:08:39 +0700

    commit final nomor 1 - task_8

--- 8_JavascriptRefereshment/Praktikum/nomor_1-2.js
+++ 8_JavascriptRefereshment/Praktikum/nomor_1-2.js
@@ -1,15 +1,20 @@
 let a = 5
 let b = "kampus merdeka"
 const nama = "budi"
-let terdaftar = false;
+let terdaftar = true //untuk menampilkan baris 17 value false tersebut akan diubah ke true agar dapat ditampilkan 
 let lengkap_arr = [a, b, nama, terdaftar]
 
 function perkenalan() {
     let asal = "indonesia"
     return console.log(
-        "perkenalkan nama saya " + nama + " nomor urut " + 
-        a + " sekarang sedang mengikuti " +
-        b + " berasal dari " + asal
+        "perkenalkan nama saya " + 
+        nama + 
+        " nomor urut " + 
+        a + 
+        " sekarang sedang mengikuti " +
+        b + 
+        " berasal dari " + 
+        asal
     )
 }
 
@@ -18,8 +23,10 @@ if (terdaftar === true) {
 }
 
 a = b;
-nama = b ;
+// nama = b;   variabel 'nama' menggunakan variabel const yang dimana nilai/atau variabel yang sudah di deklarasikan tidak dapat di deklarasikan kembali.
 
-console.log("asal diakses " + asal);
+// console.log("asal diakses " + asal)   variabel asal dideklarasikan di dalam scope function 'perkenalan' dan variabel tersebut hanya dapat diakses di dalam scope function 'perkenalan' saja
+console.log("array = " + lengkap_arr[2]) // memanggil index kedua dari array pada baris ke 5
 console.log("a adalah = " + a)
 console.log("b adalah = " + b)
+perkenalan() // Memanggil Function perkenalan
