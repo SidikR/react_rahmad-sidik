2ed96b4a6185751f39a04b2abddeb3ff86ce052d
SidikR <rahmad.119140096@student.itera.ac.id>
2023-06-19 09:34:58 +0700

    commit nomor 5 - task 8

---
+++ 8_JavascriptRefereshment/Praktikum/nomor-5.js
@@ -0,0 +1,11 @@
+// nomor 5
+let value = [1, 2, 3, 4, 5, 6];
+
+console.log(value);
+
+let newValue = value.map((element) => {
+  return element * 2;
+});
+
+console.log(newValue); // Output: [2, 4, 6, 8, 10, 12]
+
